2849fa08f9e9248c65c0d304deabc4400ad6f41f
Dapower <xd102401@163.com>
2018-03-12 17:32:50 +0800

    add file
    待续。。。

---
+++ test_one/1.py
@@ -0,0 +1,24 @@
+#join()阻塞 py3.x
+import queue,time,threading
+"""Return the CPU time or real time since the start of the process or since
+    the first call to clock().  This has as much precision as the system
+    records."""
+start = time.clock()
+
+def worker(m):
+	print('worker',m)
+	time.sleep(1)
+	return
+
+if __name__ == '__main__':
+	threads = []
+	for i in range(5):
+		threads.append(threading.Thread(target=worker,args=(i,)))
+	for t in threads:
+		t.start()
+		t.join()#阻塞子程序（如何不要阻塞子程序，请删除该行）
+
+	# t.join()#阻塞父程序（如果不要阻塞父程序，请删除该行）
+
+	end = time.clock()
+	print("finished: %0.3fs" % (end-start))
